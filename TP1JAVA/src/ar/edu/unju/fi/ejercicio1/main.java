8e8b0d893f93fbcef942eba71e10deedf24be739
Mauro006 <maurodevsm23@gmail.com>
2024-04-02 16:41:37 -0300

    se agrega los paquetes y clases para el ejercicio 1 PV

---
+++ TP1JAVA/src/ar/edu/unju/fi/ejercicio1/main.java
@@ -0,0 +1,11 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola chicos");
+
+	}
+
+}
